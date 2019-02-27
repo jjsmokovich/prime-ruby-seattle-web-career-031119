d151c982b82f51cd15884b8f71e8b9447b26e79d
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-27 11:39:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,25 @@
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    num % n == 0
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+def prime?(int)
+  if int > 1
+    (2..int - 1).all? do |num|
+      int % num != 0
+    end
+  
+  else
+    return false
   end
-else
-  end
-  return false
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
