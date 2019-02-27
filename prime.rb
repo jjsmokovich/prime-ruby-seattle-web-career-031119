4a6ebe1ef785ecd53c58e5953913a4c0a1e38e43
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-27 11:27:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,8 @@ def prime?(int)
   start = 2
   
   if int > 1
-    range = (start..number-1).all?
-       do |num_to_test|
-      number % num_to_test == 0
+    range = (start..number-1).all? do |num|
+      int % num == 0
     end
   else
     false
