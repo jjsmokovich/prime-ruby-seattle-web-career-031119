15bd3dddc197da7a1e2c398b83d53b517ad45877
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-02-27 11:42:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,12 @@
 def prime?(int)
   if int > 1 
-    (2..int - 1).each
+    (2..int - 1).each do |num|
+      int % num != 0
+    end
+  else 
+    return false
+  end
+end
 
 def prime?(int)
   if int > 1
